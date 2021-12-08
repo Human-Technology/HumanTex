2e9038e3a3be10e619b699c200d29a214c376391
Human-Technology <josesanchezan99@gmail.com>
2021-12-08 17:39:38 -0600

    Se agrego el Menu junto con sus opciones

--- src/sanchez/jose/editor/Principal.java
+++ src/sanchez/jose/editor/Principal.java
@@ -22,7 +22,54 @@ class Marco extends JFrame{
 
 class Panel extends JPanel{
 	public Panel() {
-	
+		//----------- Menu ------------------------------------
+		JPanel panelMenu = new JPanel();
+		
+		menu = new JMenuBar();
+		archivo = new JMenu("Archivo");
+		editar = new JMenu("Editar");
+		seleccion = new JMenu("Selección");
+		ver = new JMenu("Ver");
+		apariencia = new JMenu("Apariencia");
+		
+		menu.add(archivo);
+		menu.add(editar);
+		menu.add(seleccion);
+		menu.add(ver);
+		
+		
+		//---------------- ELEMENTOS DEL MENU ARCHIVO---------------
+		creaItem("Nuevo Archivo", "archivo", "nuevo");
+		creaItem("Abrir Archivo", "archivo", "abrir");
+		creaItem("Guardar", "archivo", "guardar");
+		creaItem("Guardar Como", "archivo", "guardarComo");
+		//--------------------------------------------------------
+		
+		//------------------ ELEMENTOS DEL MENU EDITAR ----------------
+		creaItem("Deshacer", "editar", "");
+		creaItem("Rehacer", "editar", "");
+		editar.addSeparator();
+		creaItem("Cortar", "editar", "");
+		creaItem("Copiar", "editar", "");
+		creaItem("Pegar", "editar", "");
+		//-------------------------------------------------------------
+		
+		//----------------- ELEMENTOS DEL MENU SELECCION ---------------
+		creaItem("Seleccionar Todo", "seleccion", "");
+		//--------------------------------------------------------------
+		
+		//------------------- ELEMENTOS DEL MENU VER -------------------
+		creaItem("Numeracion", "ver", "");
+		ver.add(apariencia);
+		creaItem("Normal", "apariencia", "");
+		creaItem("Dark", "apariencia", "");
+		//--------------------------------------------------------------
+		
+		
+		panelMenu.add(menu);
+		//-----------------------------------------------------
+		
+		
 		//----------- Area de Texto --------------------------
 		tPane = new JTabbedPane();
 		
@@ -30,7 +77,29 @@ class Panel extends JPanel{
 		
 		creaPanel();
 		
+		add(panelMenu);
 		add(tPane);
+	}
+	
+	public void creaItem(String rotulo, String menu, String accion) {
+		elementoItem = new JMenuItem(rotulo);
+		
+		if(menu.equals("archivo")) {
+			archivo.add(elementoItem);
+		}
+		else if(menu.equals("editar")) {
+			editar.add(elementoItem);
+		}
+		else if(menu.equals("seleccion")) {
+			seleccion.add(elementoItem);
+		}
+		else if(menu.equals("ver")) {
+			ver.add(elementoItem);
+		}
+		else if(menu.equals("apariencia")) {
+			apariencia.add(elementoItem);
+		}
+		
 	}
 	
 	public void creaPanel() {
@@ -46,7 +115,9 @@ class Panel extends JPanel{
 	private JTabbedPane tPane;
 	private JPanel ventana;
 	private JTextPane areaTexto;
-	
+	private JMenuBar menu;
+	private JMenu archivo, editar, seleccion, ver, apariencia;
+	private JMenuItem elementoItem;
 }
 
 
